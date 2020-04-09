a977c09d4d4b22a44a18f97194f67aa61a4826cf
Joan Marcè i Igual <j.marce.igual@gmail.com>
2020-04-09 17:58:27 +0200

    Fix space in commented variable

--- include/config.h
+++ include/config.h
@@ -2,7 +2,7 @@
 #define CONFIG_H
 
 // DM : debug message -- disable for now
-// #define DM(x) std::cerr << x
+//#define DM(x) std::cerr << x
 #define DM(x)
 
 // #define CONFIG_COLLECT_SCHEDULE_GRAPH
